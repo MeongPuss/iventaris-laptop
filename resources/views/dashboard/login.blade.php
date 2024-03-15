2be26bb0bc828eb3bc6365b6ef4e15c42c381145
MeongPuss <copoesbreaker@gmail.com>
2024-03-15 14:20:39 +0700

    halaman ui login

--- resources/views/dashboard/login.blade.php
+++ resources/views/dashboard/login.blade.php
@@ -1,24 +1,106 @@
-<form action="{{ route('dashboard.authuser') }}" method="post">
-    @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-    <h1>Login</h1>
+<head>
+    <meta charset="utf-8" />
+    <title>Halaman Panel Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
 
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" value="{{old('email')}}">
+    <!-- App css -->
+    <link href="{{ asset('assets/css/bootstrap-creative.min.css') }}" rel="stylesheet" type="text/css"
+        id="bs-default-stylesheet" />
+    <link href="{{ asset('assets/css/app-creative.min.css') }}" rel="stylesheet" type="text/css"
+        id="app-default-stylesheet" />
+
+    <link href="{{ asset('assets/css/bootstrap-creative-dark.min.css') }}" rel="stylesheet" type="text/css"
+        id="bs-dark-stylesheet" disabled />
+    <link href="{{ asset('assets/css/app-creative-dark.min.css') }}" rel="stylesheet" type="text/css"
+        id="app-dark-stylesheet" disabled />
+
+    <!-- icons -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="authentication-bg authentication-bg-pattern">
+
+    <div class="account-pages mt-5 mb-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-pattern">
+
+                        <div class="card-body p-4">
+
+                            <div class="text-center w-75 m-auto">
+                                <p class="mb-4 mt-3"><b>Halaman Panel Login</b></p>
+                            </div>
+                            @if ($errors->any())
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
+
+                            <form action="{{ route('dashboard.authuser') }}" method="POST">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <label for="email">Email address</label>
+                                    <input class="form-control" type="email" id="email" name="email" placeholder="Enter your email" value="{{ old('email') }}">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="password">Password</label>
+                                    <div class="input-group input-group-merge">
+                                        <input type="password" id="password" class="form-control" name="password" placeholder="Enter your password">
+                                        <div class="input-group-append" data-password="false">
+                                            <div class="input-group-text">
+                                                <span class="password-eye"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-0 text-center">
+                                    <button class="btn btn-primary btn-block" type="submit"> Masuk </button>
+                                </div>
+
+                            </form>
+
+                        </div> <!-- end card-body -->
+                    </div>
+                    <!-- end card -->
+                    <!-- end row -->
+
+                </div> <!-- end col -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
     </div>
-    <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
-    </div>
+    <!-- end page -->
 
-    <button type="submit">Login</button>
-</form>
+
+    <footer class="footer footer-alt text-white-50">
+        2015 -
+        <script>
+            document.write(new Date().getFullYear())
+        </script> &copy; UBold theme by <a href="" class="text-white-50">Coderthemes</a>
+    </footer>
+
+    <!-- Vendor js -->
+    <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+
+</body>
+
+</html>
